8d4a749495bdebaa6c5181d0e815c26931cd390e
jaimemiyamoto-lang <jaimemiyamoto@gmail.com>
2025-12-25 18:07:31 -0500

    Add files via upload

---
+++ scriot.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(() => {
+        document.querySelector('.gas-effect').style.opacity = '0.5';
+        document.querySelector('.shooting-star').style.opacity = '1';
+    }, 2000); // Start effects after 2 seconds
+};
